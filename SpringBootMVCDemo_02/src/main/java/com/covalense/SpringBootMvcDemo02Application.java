02f08f4ae88df3922b9a2edd675698d8a53b52cd
amrit.lal <amrit.lal@AmritLal.covalensedigital.net>
2021-11-17 17:23:14 +0530

    Commit 1 : Creating a SpringBoot Project

---
+++ SpringBootMVCDemo_02/src/main/java/com/covalense/SpringBootMvcDemo02Application.java
@@ -0,0 +1,13 @@
+package com.covalense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcDemo02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcDemo02Application.class, args);
+	}
+
+}
